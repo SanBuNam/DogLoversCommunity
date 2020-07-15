5d41fe029f99ed72840834befa3bc3384b9f1322
David Lee <david.lee@corp.nmsigroup.com>
2020-07-15 13:16:10 -0700

    frontend setup done

--- src/app/components/TaskDetail.jsx
+++ src/app/components/TaskDetail.jsx
@@ -9,17 +9,20 @@ const taskDetail = ({
   task,
   isComplete,
   groups,
+
   setTaskCompletion,
+  setTaskName,
+  setTaskGroup,
 }) => (
   <div>
-    <input value={task.name} />
+    <input onChange={setTaskName} value={task.name} />
     <div>
       <button onClick={() => setTaskCompletion(id, !isComplete)}>
         {isComplete ? `Reopen` : `Complete`}
       </button>
     </div>
     <div>
-      <select>
+      <select onChange={setTaskGroup} value={task.group}>
         {groups.map((group) => (
           <option key={group.id} value={group.id}>
             {group.name}
@@ -53,6 +56,12 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     setTaskCompletion(id, isComplete) {
       dispatch(mutations.setTaskCompletion(id, isComplete));
+    },
+    setTaskGroup(e) {
+      dispatch(mutations.setTaskGroup(id, e.target.value));
+    },
+    setTaskName(e) {
+      dispatch(mutations.setTaskName(id, e.target.value));
     },
   };
 };
