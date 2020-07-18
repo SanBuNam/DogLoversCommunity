fd5ac6a047c1bdb2f13227b218c8db5a0915876c
sanbunam <softdavidlee@gmail.com>
2020-07-17 22:01:41 -0700

    Setting up express server

--- src/app/store/sagas.mock.js
+++ src/app/store/sagas.mock.js
@@ -9,10 +9,6 @@ export function* taskCreationSaga() {
     const ownerID = `U1`;
     const taskID = uuid();
     yield put(mutations.createTask(taskID, groupID, ownerID));
-<<<<<<< HEAD
-
-=======
->>>>>>> 6c2381503c07afff18232089cf8eaec6cdbc7f42
     console.log("Got group ID", groupID);
   }
 }
